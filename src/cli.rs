899b6bcef7f2281ac385df951fe4b479d767e689
HAHWUL <hahwul@gmail.com>
2025-12-08 23:30:20 +0900

    Update cli.rs

--- src/cli.rs
+++ src/cli.rs
@@ -4,7 +4,7 @@ use clap::Parser;
 #[derive(Parser, Debug)]
 #[command(author, version, about, long_about = None)]
 pub struct Cli {
-    /// Target URLs (supports multiple URLs)
+    /// Target URLs
     pub urls: Vec<String>,
 
     /// Custom method for the attack request
